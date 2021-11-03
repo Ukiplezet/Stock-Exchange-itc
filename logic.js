1cc4539affbff5a8be4f2175ebcf5a1b7fb2580e
Omri Dolberg <88512557+Ukiplezet@users.noreply.github.com>
2021-11-03 16:45:35 +0200

    m4-F: improved design

--- logic.js
+++ logic.js
@@ -2,6 +2,7 @@ const spinner = document.getElementById("stockSpinner");
 const button = document.getElementById(`searchButton`);
 const header = document.getElementById(`header`);
 const marqueeWrap = document.getElementById(`marqueeWrap`);
+const companiesContent = document.getElementById(`companiesContent`);
 
 CurrentStockMarquee();
 
@@ -24,6 +25,12 @@ button.addEventListener(`click`, async () => {
       setTimeout(() => {
         spinner.classList.add("d-none");
         listOfCompanies.classList.remove("d-none");
+        companiesContent.classList.add(
+          "bg-light",
+          "border",
+          "border-1",
+          "border-grey"
+        );
       }, 5000);
     });
 });
@@ -49,15 +56,15 @@ async function getTheProfile(data) {
     tagImg.onerror = function placeCage() {
       tagImg.src = "https://www.placecage.com/c/205/210";
     };
-    tagImg.classList.add("rounded", "mt-2", "mx-4");
-    tagImg.style = "max-height:50px; max-width:50px;";
+    tagImg.classList.add("rounded", "mt-2", "me-2");
+    tagImg.style = "max-height:25px; max-width:25px;";
     let dailyChange = document.createElement("p");
     let percentChange = profileData.profile.changesPercentage;
     percentChange = Number(percentChange).toFixed(2);
     dailyChange.innerText = `(` + percentChange + `%` + `)`;
     if (percentChange > 0) dailyChange.classList.add("text-success");
     if (percentChange < 0) dailyChange.classList.add("text-danger");
-    dailyChange.classList.add("pt-3", "mx-3");
+    dailyChange.classList.add("me-1", "mt-2", "ms-3");
     let tagLink = document.createElement("a");
     tagLink.href = ``;
     companyData.classList.add(
@@ -65,14 +72,13 @@ async function getTheProfile(data) {
       "border-grey",
       "border-0",
       "d-inline-block",
-      "px-2",
-      "fs-5"
+      "px-2"
     );
     let COMPANY_URL = "/company.html?symbol=" + data[i].symbol;
     tagLink.href = COMPANY_URL;
     let tag = document.createElement(`p`);
     tag.innerText = data[i].name + ` ` + " (" + data[i].symbol + ")";
-    tag.classList.add("pt-3", "mx-2");
+    tag.classList.add("mt-2");
     tagLink.appendChild(tag);
     companyData.append(tagImg);
     companyData.append(tagLink);
@@ -99,10 +105,10 @@ async function CurrentStockMarquee() {
       currentChange.classList.add("text-success");
     if (currentChange.innerText < 0) currentChange.classList.add("text-danger");
     let marqueeBanner = document.createElement("span");
-    companyTicker.classList.add("fs-6", "ms-3", "flex-row-nowrap");
-    currentChange.classList.add("fs-6", "mx-1", "flex-row-nowrap");
-    currentPrice.classList.add("fs-6", "ms-1", "me-1", "flex-row-nowrap");
-    marqueeBanner.classList.add("fs-6", "d-flex", "flex-row-nowrap");
+    companyTicker.classList.add("ms-3", "flex-row-nowrap");
+    currentChange.classList.add("mx-1", "flex-row-nowrap");
+    currentPrice.classList.add("ms-1", "me-1", "flex-row-nowrap");
+    marqueeBanner.classList.add("d-flex", "flex-row-nowrap");
     marqueeBanner.append(companyTicker);
     marqueeBanner.append(currentChange);
     marqueeBanner.append(currentPrice);
